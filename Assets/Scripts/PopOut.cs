73b8e5080b2b8d463fbc5b9436342f1f3e2c6dd5
GMan9506 <35857766+GMan9506@users.noreply.github.com>
2024-01-27 14:07:17 -0800

    Added a Popout prop
    Can Detach the cutout and shatter if enabled

---
+++ Assets/Scripts/PopOut.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopOut : MonoBehaviour
+{
+    Animator mAnim;
+    bool mHiding = true;
+    //Enable this if the cutout object is going to drop and shatter
+    public bool mDropCutout = false;
+    public bool mShatterobject = false;
+    public GameObject mCutout;
+
+
+    void Start()
+    {
+        mAnim = GetComponent<Animator>();
+    }
+
+
+    void Update()
+    {
+        //temporary way to activate the object. To properly use this, call the RevealPopout() from wherever it is needed
+        if(mHiding)
+            if (Input.GetKeyDown(KeyCode.Space))   
+                RevealPopOut();
+        
+    }
+
+    public void RevealPopOut()
+    {
+        mHiding = false;
+        mAnim.SetBool("Hiding", false);
+        if (mDropCutout)
+            StartCoroutine(DropWait());
+
+    }
+
+
+    IEnumerator DropWait()
+    {
+        yield return new WaitForSeconds(2);
+        DetachCutout();
+    }
+    public void DetachCutout()
+    {
+        if(mShatterobject)
+            mCutout.GetComponent<GlassShatter>().enabled = true;
+
+        mCutout.transform.SetParent(null);
+        mCutout.GetComponent<Rigidbody>().AddForce(Random.Range(1,5), 0, Random.Range(0,3));
+        mCutout.GetComponent<Rigidbody>().useGravity = true;
+        mCutout.GetComponent<SphereCollider>().enabled = true;
+    }
+}
